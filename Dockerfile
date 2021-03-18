1a99a12f63cfd1de71a3fc8ff662b5baa5a28f51
LucasLeRay <lucas.a.leray@gmail.com>
2021-03-18 14:16:51 +0100

    feat: use spark jdbc

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM godatadriven/pyspark
 
-RUN pip install psycopg2-binary
+RUN apt-get install -y wget
+ENV POST_URL https://jdbc.postgresql.org/download/postgresql-42.2.5.jar
+RUN wget ${POST_URL}
+RUN mkdir -p $SPARK_HOME/jars/
+RUN mv postgresql-42.2.5.jar $SPARK_HOME/jars/postgresql-42.2.5.jar
 
 WORKDIR /job
 COPY . .
--- main.py
+++ main.py
@@ -1,33 +1,10 @@
 from pyspark.sql import functions as sf
 from pyspark.sql import SparkSession
-import psycopg2
-
-def connect_db():
-  conn = psycopg2.connect(
-        host = "my_postgres",
-        database = "election",
-        user = "admin",
-        password = "admin")
-  
-  print('Connection to PostgreSQL established')
-  return conn
-
-def create_table(cur):
-  cur.execute("CREATE TABLE IF NOT EXISTS election_table( \
-    Coordinates VARCHAR(255) NOT NULL, \
-    Latitude FLOAT NOT NULL, \
-    Longitude FLOAT NOT NULL, \
-    Name VARCHAR(255) NOT NULL, \
-    Voted integer NOT NULL \
-  );")
-
-  print("Table created in PostgresSQL")
 
 def init_spark():
   spark = SparkSession.builder \
-    .config('spark.sql.debug.maxToStringFields', 2000) \
+    .config("spark.jars", "/jars/postgresql-42.2.5.jar") \
     .getOrCreate()
-  print('Spark initialized')
   return spark
 
 def extract(spark):
@@ -54,33 +31,18 @@ def transform(df):
   return df.select('Coordinates', 'Latitude', 'Longitude', 'Name', 'Voted')
 
 def load(df):
-  conn = connect_db()
-  cur = conn.cursor()
-  create_table(cur)
-  
-  seq = [tuple(x) for x in df.collect()]
-  records = ','.join(['%s'] * len(seq))
-  query = "INSERT INTO election_table ( \
-    Coordinates, \
-    Latitude, \
-    Longitude, \
-    Name, \
-    Voted \
-  ) VALUES {}".format(records)
-  cur.execute(query, seq)
-
-  print("Data inserted into PostgreSQL")
-
-  cur.close()
-  print("Commiting changes to database")
-  conn.commit()
-  print("Closing connection", "\n")
-  conn.close()
-  print("ETL job done!")
+  mode = "overwrite"
+  url = "jdbc:postgresql://my_postgres:5432/election"
+  properties = {"user": "admin","password": "admin","driver": "org.postgresql.Driver"}
+  df.write.jdbc(url=url, table="election_table", mode=mode, properties=properties)
 
 spark = init_spark()
-
+print('Spark initialized')
 df = extract(spark)
+print('Data extracted')
 df = transform(df)
+print('Data transformed:')
 df.show()
 load(df)
+print('Data loaded')
+print("ETL job done!")
